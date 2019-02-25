6991df881cbfff06a5ba6dfce6a747a0790fdccf
PTR <semper576@gmail.com>
2019-02-25 16:38:01 +0100

    altering first commit

--- src/Components/Carousel/carousel.js
+++ src/Components/Carousel/carousel.js
@@ -14,7 +14,6 @@ class Carousel extends Component {
         super(props);
         this.backgrounds = [bg0, bg1, bg2, bg3, bg4, bg5];
         this.state = {
-            highscore: 5,
             currentSlideId: 0,
             nextButtonDisabled: false,
         }
@@ -25,60 +24,45 @@ class Carousel extends Component {
         if (!this.state.nextButtonDisabled) {
             // Ha már letelt a 750ms-os zár
 
-            btn.afterburn = -1;
-            btn.progress = -1;
-
-
             if (!event || Number(event.target.id) !== this.state.currentSlideId) {
+                btn.afterburn = -1;
+                btn.progress = -1;
+
                 let updatedCurrentSlideId;
                 const previousSlideId = this.state.currentSlideId;
 
                 if (event) {
                     updatedCurrentSlideId = Number(event.target.id);
-
                 } else {
-                    //magától meghívódik
                     updatedCurrentSlideId = this.state.currentSlideId === this.backgrounds.length-1 ? 0 : this.state.currentSlideId + 1;
                 }
-                const updatedZIndex = this.state.highscore + 1;
 
                 const setStuff = () => {
 
-                    const $prevSlide_inner = this.refs["inner" + previousSlideId];
-                    const $prevSlide_outer = this.refs["outer" + previousSlideId];
-                    const $currentSlide_inner = this.refs["inner" + this.state.currentSlideId];
-                    const $currentSlide_outer = this.refs["outer" + this.state.currentSlideId];
+                    const $frontSide_inner = this.refs.inner0;
+                    const $backSide_inner = this.refs.inner1;
 
-
-                    $currentSlide_inner.style.transform = "translateX(0)";
-                    $currentSlide_inner.style.transition = "transform cubic-bezier(.12,.77,.33,.96) 1.5s, width 1.5s";
-                    $prevSlide_inner.style.transform = "translateX(-20%)";
-                    $prevSlide_inner.style.width = "0%";
+                    $frontSide_inner.style.transition = "width 0s, transform 0s";
+                    $backSide_inner.style.transition = "width 0s, transform 0s";
+                    $frontSide_inner.style.transform = "translateX(0%)";
+                    // $frontSide_inner.style.width = "100%";
+                    this.refs.img0.setAttribute("src", this.backgrounds[previousSlideId]);
+                    this.refs.img1.setAttribute("src", this.backgrounds[this.state.currentSlideId]);
+                    $backSide_inner.style.transform = "translateX(10%)";
 
                     setTimeout(function () {
-
-                        $prevSlide_inner.style.transition = "transform 0s";
-                        $prevSlide_inner.style.transform = "translateX(20%)";
-                        $prevSlide_inner.style.width = "100%";
-
-
-                        $prevSlide_outer.style.highscore = this.state.zIndex;
-
-                        this.setState({
-                            nextButtonDisabled: false
-                        });
-
-                    }.bind(this), 1500)
-                }
+                        $frontSide_inner.style.transition = "all cubic-bezier(.1,.6,.3,.96) 1.5s";
+                        $backSide_inner.style.transition = "all cubic-bezier(.1,.6,.3,.96) 1.5s";
+                        $frontSide_inner.style.transform = "translateX(-100%)";
+                        // $frontSide_inner.style.width = "0%";
+                        $backSide_inner.style.transform = "translateX(0%)";
+                    }, 30)
+                };
 
                 this.setState({
                     currentSlideId: updatedCurrentSlideId,
-                    nextButtonDisabled: true,
-                    highscore: updatedZIndex
                 }, setStuff);
-
             }
-
         }
     }
 
@@ -95,7 +79,6 @@ class Carousel extends Component {
 
     render() {
 
-
         // if (this.props.scrolled) {
         //     btn.stop()
         // }
@@ -107,43 +90,19 @@ class Carousel extends Component {
 
                     <div ref="outer0" className={`lead ${this.props.scrolled ? "shrinkedLeadWrapper" : ""}`} style={{zIndex: "5"}}>
                         <div ref="inner0" className="container" style={{transform: "translateX(0%)"}}>
-                            <img className="image" src={this.backgrounds[0]} style={{visibility: "visible"}} alt="lead0" />
+                            <img ref="img0" className="image" src={this.backgrounds[0]} alt="lead0" />
                         </div>
                     </div>
 
                     <div ref="outer1" className={`lead ${this.props.scrolled ? "shrinkedLeadWrapper" : ""}`} style={{zIndex: "4"}}>
-                        <div ref="inner1" className="container" style={{transform: "translateX(20%)"}}>
-                            <img className="image" src={this.backgrounds[1]} style={{visibility: "visible"}} alt="lead1" />
-                        </div>
-                    </div>
-
-                    <div ref="outer2" className={`lead ${this.props.scrolled ? "shrinkedLeadWrapper" : ""}`} style={{zIndex: "3"}}>
-                        <div ref="inner2" className="container" style={{transform: "translateX(20%)"}}>
-                            <img className="image" src={this.backgrounds[2]} style={{visibility: "visible"}} alt="lead2" />
-                        </div>
-                    </div>
-
-                    <div ref="outer3" className={`lead ${this.props.scrolled ? "shrinkedLeadWrapper" : ""}`} style={{zIndex: "2"}}>
-                        <div ref="inner3" className="container" style={{transform: "translateX(20%)"}}>
-                            <img className="image" src={this.backgrounds[3]} style={{visibility: "visible"}} alt="lead3" />
-                        </div>
-                    </div>
-
-                    <div ref="outer4" className={`lead ${this.props.scrolled ? "shrinkedLeadWrapper" : ""}`} style={{zIndex: "1"}}>
-                        <div ref="inner4" className="container" style={{transform: "translateX(20%)"}}>
-                            <img className="image" src={this.backgrounds[4]} style={{visibility: "visible"}} alt="lead4" />
-                        </div>
-                    </div>
-
-                    <div ref="outer5" className={`lead ${this.props.scrolled ? "shrinkedLeadWrapper" : ""}`} style={{zIndex: "0"}}>
-                        <div ref="inner5" className="container" style={{transform: "translateX(20%)"}}>
-                            <img className="image" src={this.backgrounds[5]} style={{visibility: "visible"}} alt="lead5" />
+                        <div ref="inner1" className="container" style={{transform: "translateX(10%)"}}>
+                            <img ref="img1" className="image" src={this.backgrounds[1]} alt="lead1" />
                         </div>
                     </div>
 
                 </div>
 
-                <div className={styles.carouselButtons_wrapper}>
+                <div className={`${styles.carouselButtons_wrapper} ${this.props.scrolled ? styles.hiddenControls : ""}`}>
 
                     <CircleButton id="0" activeSlideId={this.state.currentSlideId} click={(e) => this.loadNext(e)} />
                     <CircleButton id="1" activeSlideId={this.state.currentSlideId} click={(e) => this.loadNext(e)} />
